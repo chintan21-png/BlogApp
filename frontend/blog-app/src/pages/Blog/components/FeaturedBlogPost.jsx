0d5d8481e0e942697940ad992ede5a31e4617359
Chintan <chintanwork21@gmail.com>
2025-12-11 19:43:18 +0530

    Pending BlogLandingPage.jsx

---
+++ frontend/blog-app/src/pages/Blog/components/FeaturedBlogPost.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FeaturedBlogPost = ({
+    title,
+    coverImageUrl,
+    description,
+    tags,
+    updatedOn,
+    authorName,
+    authPorfileImg,
+    onClick,
+}) => {
+  return (
+    <div>FeaturedBlogPost</div>
+  )
+}
+
+export default FeaturedBlogPost
